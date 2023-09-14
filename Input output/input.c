a7315b7cfc3a2a2af763daae9722cc47a14fc4ea
Erfan Hossain <111767523+ErfanHossain@users.noreply.github.com>
2023-09-14 19:47:35 +0600

    Add files via upload

---
+++ Input output/input.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+    int N;//int(integer) is data type name & N is a variable
+    scanf("%d",&N);//scanf is input system in c language we take input with scanf and %d it mean intger type
+    printf("%d",N);//printf used in c language for given output
+
+}
