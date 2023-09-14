4cb53b31e197d0e05c11586b716b7cbc92377b08
Erfan Hossain <111767523+ErfanHossain@users.noreply.github.com>
2023-09-14 20:46:07 +0600

    Update input.c

--- Input output/input.c
+++ Input output/input.c
@@ -4,5 +4,11 @@ void main()//its a main function in c language.Every lenguage have a main functi
     int N;//int(integer) is data type name & N is a variable
     scanf("%d",&N);//scanf is input system in c language we take input with scanf and %d it mean intger type
     printf("%d",N);//printf used in c language for given output
+    float A;
+    scanf("%f",&A);
+    Printf("%f",A);
+    double B;
+    scanf("%lf",&B);
+    printf("%lf",B);
 
 }
